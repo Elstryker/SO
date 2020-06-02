0ad3a74edb69d9d408423d62706a3b20dd0f7aa4
Elstryker <rubencorreiacerqueira@gmail.com>
2020-06-02 17:48:06 +0100

    Exec

--- functions.h
+++ functions.h
@@ -1,4 +1,13 @@
 #ifndef functions_h
 #define functions_h
 
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* mySep(char* tok, char* buf, char delim);
+int executar(char* buf);
+
 #endif
