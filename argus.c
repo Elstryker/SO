c44dd6485b7b1f3c1456144259b6733fe88c0918
Elstryker <rubencorreiacerqueira@gmail.com>
2020-06-15 22:30:29 +0100

    LOL

--- argus.c
+++ argus.c
@@ -40,11 +40,9 @@ void shellInterpreter(int fdToServer) {
     int flag = 1;
     buf = malloc(100 * sizeof(char));
     option = malloc(25 * sizeof(char));
-    printf("Main pid: %d\n",getpid());
     while((bytesRead = read(0,buf,100)) > 0) {
         if(compareNString(buf,"sair\n",5)== 0) break;
         if(fork() == 0) {
-            printf("childPid : %d\n",getpid());
             flag = 1;
             buf = separateString(option,buf,&bytesRead);
             // Validação do comando lido
